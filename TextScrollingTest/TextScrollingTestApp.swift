466c1244c6bd60ad19885fd93d3306cfb3070e00
Jo Lingenfelter <joanna.lingenfelter@willowtreeapps.com>
2023-04-14 12:40:53 -0400

    Initial Commit

---
+++ TextScrollingTest/TextScrollingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextScrollingTestApp.swift
+//  TextScrollingTest
+//
+//  Created by Jo Lingenfelter on 4/14/23.
+//
+
+import SwiftUI
+
+@main
+struct TextScrollingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
